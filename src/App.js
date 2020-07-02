845fdff6ed375970436b438eab5bfb0f05ae0f87
SITANSHU KUMAR <41771170+sitanshu-kumar-vst-au4@users.noreply.github.com>
2020-07-02 10:16:54 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment, createRef } from "react";
+
+const styled = {
+  container: {
+    margin: "1em",
+    width: "90%",
+  },
+  form: {
+    margin: "2em 0em",
+  },
+  input: {
+    width: "20%",
+    padding: "2% 1%",
+    margin: "0em 0.2em",
+  },
+};
 
 function App() {
+  const [state, setState] = useState({
+    first: "",
+    second: "",
+    third: "",
+    fourth: "",
+  });
+  let keysForInput = Object.keys(state);
+  const inputRefs = [createRef(), createRef(), createRef(), createRef()];
+  const changeIndex = (id, e) => {
+    let { name, value } = e.target;
+    if (value.length <= 4) {
+      setState({ ...state, [name]: value });
+      if (e.keyCode !== 8 && state[name].length === 3) {
+        if (id < inputRefs.length - 1) {
+          inputRefs[id + 1].focus();
+        }
+      }
+      if (e.keyCode === 8 && !state[name].length) {
+        if (id > 0) {
+          inputRefs[id - 1].focus();
+        }
+      }
+    } else {
+      setState({
+        ...state,
+        [keysForInput[0]]: value.substring(0, 4),
+        [keysForInput[1]]: value.substring(4, 8),
+        [keysForInput[2]]: value.substring(8, 12),
+        [keysForInput[3]]: value.substring(12, 16),
+      });
+    }
+  };
+
+  useEffect(() => {
+    inputRefs[0].focus();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div style={styled.container}>
+        <h4>Card Number*</h4>
+        <form style={styled.form}>
+          {keysForInput.map((v, i) => (
+            <input
+              key={i}
+              style={styled.input}
+              defaultValue={state[v]}
+              name={v}
+              ref={(r) => (inputRefs[i] = r)}
+              onKeyUp={(e) => changeIndex(i, e)}
+              // onPaste={(e) => divideIntoFourEach(e.target.value)}
+              // maxLength="4"
+            />
+          ))}
+        </form>
+      </div>
+    </Fragment>
   );
 }
 
